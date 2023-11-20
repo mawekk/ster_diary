bc1d78d628497a09a4b3679089ab767fd7340ccb
mawekk <mariamya243@gmail.com>
2023-11-20 16:56:20 +0300

    update adapter

--- app/src/main/java/com/mawekk/sterdiary/NoteAdapter.kt
+++ app/src/main/java/com/mawekk/sterdiary/NoteAdapter.kt
@@ -3,60 +3,77 @@ package com.mawekk.sterdiary
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
+import com.mawekk.sterdiary.databinding.DividerBinding
 import com.mawekk.sterdiary.databinding.NoteBinding
 import com.mawekk.sterdiary.db.entities.Note
 
-class NoteAdapter(val listener: (Note) -> Unit) : RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
-    private var notes = emptyList<Note>()
-    private lateinit var onClickListener: OnClickListener
+open class RecyclerViewItem
+class NoteItem(val note: Note) : RecyclerViewItem() 
+class DividerItem(val text: String) : RecyclerViewItem()
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class NoteAdapter(val listener: (NoteItem) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var data = emptyList<RecyclerViewItem>()
+
+    private val viewTypeNote = 1
+    private val viewTypeDivider = 2
+
+    override fun getItemViewType(position: Int): Int {
+        if (data[position] is NoteItem) {
+            return viewTypeNote
+        }
+        return viewTypeDivider
+    }
+
+    class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val binding = NoteBinding.bind(itemView)
-        fun bind(nextNote: Note, previousNote: Note?) {
+        fun bind(item: NoteItem) {
             binding.apply {
-                val previousDateString = when (previousNote) {
-                    null -> ""
-                    else -> previousNote.date.drop(3)
-                }
-                val nextDateString = nextNote.date.trimStart('0')
-                date.text = nextDateString.dropLast(5) + ", " + nextNote.time
-                situation.text = nextNote.situation
-                divider.text = nextNote.date.drop(3)
-                divider.isVisible =
-                    !(divider.text.toString() == previousDateString || previousNote == null)
+                val dateString = item.note.date.trimStart('0')
+                date.text = dateString.dropLast(5) + ", " + item.note.time
+                situation.text = item.note.situation
             }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.note, parent, false)
-        return ViewHolder(itemView)
-    }
-
-    override fun getItemCount() = notes.size
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val previousNote = when (position > 0) {
-            true -> notes[position - 1]
-            else -> null
+    class DividerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val binding = DividerBinding.bind(itemView)
+        fun bind(item: DividerItem) {
+            binding.divider.text = item.text
         }
-        holder.bind(notes[position], previousNote)
-        holder.itemView.setOnClickListener { listener(notes[position]) }
     }
 
-    fun setList(list: List<Note>) {
-        notes = list.reversed()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            viewTypeNote -> {
+                val itemView =
+                    LayoutInflater.from(parent.context).inflate(R.layout.note, parent, false)
+                NoteViewHolder(itemView)
+            }
+
+            else -> {
+                val itemView =
+                    LayoutInflater.from(parent.context).inflate(R.layout.divider, parent, false)
+                DividerViewHolder(itemView)
+            }
+        }
+    }
+
+    override fun getItemCount() = data.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = data[position]
+        if (holder is NoteViewHolder && item is NoteItem) {
+            holder.itemView.setOnClickListener { listener(item) }
+            holder.bind(item)
+        }
+        if (holder is DividerViewHolder && item is DividerItem) {
+            holder.bind(item)
+        }
+    }
+    
+    fun setList(list: List<RecyclerViewItem>) {
+        data = list.reversed()
         notifyDataSetChanged()
     }
-
-    fun setOnClickListener(onClickListener: OnClickListener) {
-        this.onClickListener = onClickListener
-    }
-
-    interface OnClickListener {
-        fun onClick(note: Note)
-    }
-
 }
--- app/src/main/java/com/mawekk/sterdiary/fragments/ArchiveFragment.kt
+++ app/src/main/java/com/mawekk/sterdiary/fragments/ArchiveFragment.kt
@@ -8,11 +8,13 @@ import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.mawekk.sterdiary.DividerItem
 import com.mawekk.sterdiary.MainActivity
 import com.mawekk.sterdiary.NoteAdapter
+import com.mawekk.sterdiary.NoteItem
 import com.mawekk.sterdiary.R
+import com.mawekk.sterdiary.RecyclerViewItem
 import com.mawekk.sterdiary.databinding.FragmentArchiveBinding
-import com.mawekk.sterdiary.db.entities.Note
 import com.mawekk.sterdiary.db.DiaryViewModel
 import java.text.SimpleDateFormat
 
@@ -20,7 +22,7 @@ import java.text.SimpleDateFormat
 class ArchiveFragment : Fragment() {
     private lateinit var binding: FragmentArchiveBinding
     private val viewModel: DiaryViewModel by activityViewModels()
-    private val noteAdapter = NoteAdapter { note: Note -> noteOnClick(note) }
+    private val noteAdapter = NoteAdapter { item: NoteItem -> noteOnClick(item) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,23 +40,41 @@ class ArchiveFragment : Fragment() {
             adapter = noteAdapter
         }
         viewModel.getAllNotes()
-            .observe(viewLifecycleOwner) { noteAdapter.setList(it.sortedBy { note ->
-                val dateAndTimeString = note.date + ' ' + note.time
-                val formatter = SimpleDateFormat("dd MMMM yyyy HH:mm")
-                val dateAndTime = formatter.parse(dateAndTimeString)
-                dateAndTime
-            }) }
+            .observe(viewLifecycleOwner) {
+                val items = mutableListOf<RecyclerViewItem>()
+                val notes = it.sortedBy { note ->
+                    val dateAndTimeString = note.date + ' ' + note.time
+                    val formatter = SimpleDateFormat("dd MMMM yyyy HH:mm")
+                    val dateAndTime = formatter.parse(dateAndTimeString)
+                    dateAndTime
+                }
+
+                if (notes.isNotEmpty()) {
+                    items.add(NoteItem(notes[0]))
+                    for (i in 1 until notes.size) {
+                        val dividerText = notes[i - 1].date.drop(3)
+                        val nextText = notes[i].date.drop(3)
+                        if (dividerText != nextText) {
+                            items.add(DividerItem(dividerText))
+                        }
+                        items.add(NoteItem(notes[i]))
+                    }
+                }
+
+                noteAdapter.setList(items)
+            }
     }
 
-    private fun noteOnClick(note: Note) {
+    private fun noteOnClick(item: NoteItem) {
         val activity = activity as MainActivity
         activity.binding.apply {
             topAppBar.isVisible = false
             noteTopBar.isVisible = true
         }
         activity.hideBottomNavigation()
-        viewModel.selectNote(note)
+        viewModel.selectNote(item.note)
         activity.showFragment(NoteFragment.newInstance(), R.id.addNoteButton)
+
     }
 
     companion object {
