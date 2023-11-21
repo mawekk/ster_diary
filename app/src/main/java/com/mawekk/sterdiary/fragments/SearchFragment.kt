2ba27b4fb8a8758dce99c915c823f72d6e8d3b3d
mawekk <mariamya243@gmail.com>
2023-11-21 17:44:28 +0300

    add search

--- app/src/main/java/com/mawekk/sterdiary/fragments/SearchFragment.kt
+++ app/src/main/java/com/mawekk/sterdiary/fragments/SearchFragment.kt
@@ -5,16 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mawekk.sterdiary.R
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.activityViewModels
+import com.mawekk.sterdiary.RecyclerViewWorker
+import com.mawekk.sterdiary.databinding.FragmentSearchBinding
+import com.mawekk.sterdiary.db.DiaryViewModel
 
 class SearchFragment : Fragment() {
+    private lateinit var binding: FragmentSearchBinding
+    private val viewModel: DiaryViewModel by activityViewModels()
+    private lateinit var worker: RecyclerViewWorker
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        worker = RecyclerViewWorker(
+            binding.searchRecyclerView,
+            requireActivity(),
+            viewModel,
+            viewLifecycleOwner
+        )
+
+        worker.initRecyclerView()
+        worker.setRecyclerViewItemsForSearch()
+        setSearchViewAction()
+        return binding.root
     }
 
+    private fun setSearchViewAction() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                worker.filterNotes(newText)
+                return true
+            }
+
+        })
+    }
 
     companion object {
         @JvmStatic
